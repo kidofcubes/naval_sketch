17d38ccc160634f3fb1d6151b61754daabc52226
kidofcubes <kidofcubes@gmail.com>
2025-01-15 15:04:31 +0800

    rahh

---
+++ src/cam_movement.rs
@@ -0,0 +1,207 @@
+use bevy::{asset::RenderAssetUsages, color::Color, input::mouse::AccumulatedMouseMotion, pbr::ScreenSpaceAmbientOcclusion, prelude::*, render::mesh::Indices, window::CursorGrabMode};
+use std::{env, f32::consts::FRAC_PI_2};
+
+/// A vector representing the player's input, accumulated over all frames that ran
+/// since the last time the physics simulation was advanced.
+#[derive(Debug, Component, Clone, Copy, PartialEq, Default, Deref, DerefMut)]
+pub struct AccumulatedInput(Vec3);
+
+/// A vector representing the player's velocity in the physics simulation.
+#[derive(Debug, Component, Clone, Copy, PartialEq, Default, Deref, DerefMut)]
+pub struct Velocity(Vec3);
+
+/// The actual position of the player in the physics simulation.
+/// This is separate from the `Transform`, which is merely a visual representation.
+///
+/// If you want to make sure that this component is always initialized
+/// with the same value as the `Transform`'s translation, you can
+/// use a [component lifecycle hook](https://docs.rs/bevy/0.14.0/bevy/ecs/component/struct.ComponentHooks.html)
+#[derive(Debug, Component, Clone, Copy, PartialEq, Default, Deref, DerefMut)]
+pub struct PhysicalTranslation(Vec3);
+
+/// The value [`PhysicalTranslation`] had in the last fixed timestep.
+/// Used for interpolation in the `interpolate_rendered_transform` system.
+#[derive(Debug, Component, Clone, Copy, PartialEq, Default, Deref, DerefMut)]
+pub struct PreviousPhysicalTranslation(Vec3);
+
+/// Spawn the player sprite and a 2D camera.
+pub fn spawn_player(mut commands: Commands) {
+    let mut camera = Camera3d{
+            
+            ..default()
+        };
+    commands.spawn((
+        camera,
+        Msaa::Off,
+        Name::new("Player"),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        AccumulatedInput::default(),
+        Velocity::default(),
+        PhysicalTranslation::default(),
+        PreviousPhysicalTranslation::default(),
+        ScreenSpaceAmbientOcclusion::default(),
+    ));
+}
+
+/// Spawn a bit of UI text to explain how to move the player.
+pub fn spawn_text(mut commands: Commands) {
+    commands
+        .spawn(Node {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(12.0),
+            left: Val::Px(12.0),
+            ..default()
+        })
+        .with_child((
+            Text::new("Move the player with WASD"),
+            TextFont {
+                font_size: 25.0,
+                ..default()
+            },
+        ));
+}
+
+/// Handle keyboard input and accumulate it in the `AccumulatedInput` component.
+///
+/// There are many strategies for how to handle all the input that happened since the last fixed timestep.
+/// This is a very simple one: we just accumulate the input and average it out by normalizing it.
+pub fn handle_input(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut AccumulatedInput, &mut Velocity, &mut Transform)>,
+) {
+    /// Since Bevy's default 2D camera setup is scaled such that
+    /// one unit is one pixel, you can think of this as
+    /// "How many pixels per second should the player move?"
+    const SPEED: f32 = 100.0;
+    for (mut input, mut velocity, mut transform) in query.iter_mut() {
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            input.z -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            input.z += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            input.x -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            input.x += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::Space) {
+            input.y += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::ShiftLeft) {
+            input.y -= 1.0;
+        }
+
+        // Need to normalize and scale because otherwise
+        // diagonal movement would be faster than horizontal or vertical movement.
+        // This effectively averages the accumulated input.
+        velocity.0 = transform.rotation.mul_vec3(input.normalize_or_zero() * SPEED);
+    }
+}
+
+/// Advance the physics simulation by one fixed timestep. This may run zero or multiple times per frame.
+///
+/// Note that since this runs in `FixedUpdate`, `Res<Time>` would be `Res<Time<Fixed>>` automatically.
+/// We are being explicit here for clarity.
+pub fn advance_physics(
+    fixed_time: Res<Time<Fixed>>,
+    mut query: Query<(
+        &mut PhysicalTranslation,
+        &mut PreviousPhysicalTranslation,
+        &mut AccumulatedInput,
+        &Velocity,
+    )>,
+) {
+    for (
+        mut current_physical_translation,
+        mut previous_physical_translation,
+        mut input,
+        velocity,
+    ) in query.iter_mut()
+    {
+        previous_physical_translation.0 = current_physical_translation.0;
+        current_physical_translation.0 += velocity.0 * fixed_time.delta_secs();
+
+        // Reset the input accumulator, as we are currently consuming all input that happened since the last fixed timestep.
+        input.0 = Vec3::ZERO;
+    }
+}
+
+pub fn interpolate_rendered_transform(
+    fixed_time: Res<Time<Fixed>>,
+    mut query: Query<(
+        &mut Transform,
+        &PhysicalTranslation,
+        &PreviousPhysicalTranslation,
+    )>,
+) {
+    for (mut transform, current_physical_translation, previous_physical_translation) in
+        query.iter_mut()
+    {
+        let previous = previous_physical_translation.0;
+        let current = current_physical_translation.0;
+        // The overstep fraction is a value between 0 and 1 that tells us how far we are between two fixed timesteps.
+        let alpha = fixed_time.overstep_fraction();
+
+        let rendered_translation = previous.lerp(current, alpha);
+        transform.translation = rendered_translation;
+    }
+}
+
+pub fn move_player(
+    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
+    mut player: Query<(&mut Transform), With<Camera3d>>,
+) {
+    let Ok(mut transform) = player.get_single_mut() else {
+        return;
+    };
+
+    let delta = accumulated_mouse_motion.delta;
+
+    if delta != Vec2::ZERO {
+        // Note that we are not multiplying by delta_time here.
+        // The reason is that for mouse movement, we already get the full movement that happened since the last frame.
+        // This means that if we multiply by delta_time, we will get a smaller rotation than intended by the user.
+        // This situation is reversed when reading e.g. analog input from a gamepad however, where the same rules
+        // as for keyboard input apply. Such an input should be multiplied by delta_time to get the intended rotation
+        // independent of the framerate.
+        let delta_yaw = -delta.x * 0.005;
+        let delta_pitch = -delta.y * 0.005;
+
+        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+        let yaw = yaw + delta_yaw;
+
+        // If the pitch was ±¹⁄₂ π, the camera would look straight up or down.
+        // When the user wants to move the camera back to the horizon, which way should the camera face?
+        // The camera has no way of knowing what direction was "forward" before landing in that extreme position,
+        // so the direction picked will for all intents and purposes be arbitrary.
+        // Another issue is that for mathematical reasons, the yaw will effectively be flipped when the pitch is at the extremes.
+        // To not run into these issues, we clamp the pitch to a safe range.
+        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
+
+        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+    }
+}
+
+pub fn grab_mouse(
+    mut windows: Query<&mut Window>,
+    mouse: Res<ButtonInput<MouseButton>>,
+    key: Res<ButtonInput<KeyCode>>,
+) {
+    let mut window = windows.single_mut();
+
+    if mouse.just_pressed(MouseButton::Left) {
+        window.cursor_options.visible = false;
+        window.cursor_options.grab_mode = CursorGrabMode::Locked;
+    }
+
+    if key.just_pressed(KeyCode::Escape) {
+        window.cursor_options.visible = true;
+        window.cursor_options.grab_mode = CursorGrabMode::None;
+    }
+}
+
+
+
---
+++ src/parsing.rs
@@ -0,0 +1,271 @@
+use core::str;
+use std::{borrow::BorrowMut, error::Error, fmt::Display, fs};
+
+use bevy::{color::Color, math::Vec3, prelude::Component};
+use quick_xml::{events::{BytesStart, Event}, Reader};
+use regex::Regex;
+
+#[derive(Component, Debug, Copy, Clone)]
+pub struct BasePart {
+    pub id: i32,
+    pub ignore_physics: bool,
+    pub position: Vec3,
+    pub rotation: Vec3,
+    pub scale: Vec3,
+    pub color: Color,
+    pub armor: i32,
+}
+
+#[derive(Component, Debug, Copy, Clone)]
+pub struct AdjustableHull {
+    pub length: f32,
+    pub height: f32,
+    pub front_width: f32,
+    pub back_width: f32,
+    pub front_spread: f32,
+    pub back_spread: f32,
+    pub top_roundness: f32,
+    pub bottom_roundness: f32,
+    pub height_scale: f32,
+    pub height_offset: f32,
+}
+
+#[derive(Component, Debug, Copy, Clone)]
+pub struct Turret{
+    pub manual_control: bool,
+    pub elevator: f32,
+}
+
+
+impl Default for BasePart {
+    fn default() -> BasePart {
+        BasePart {
+            id: 0,
+            ignore_physics: false,
+            position: Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            rotation: Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            scale: Vec3 {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0,
+            },
+            color: Color::srgb(5.0, 5.0, 5.0),
+            armor: 0,
+        }
+    }
+}
+
+
+impl Default for AdjustableHull{
+    fn default() -> AdjustableHull {
+        AdjustableHull {
+            length:0.0,
+            height:0.0,
+            front_width:0.0,
+            back_width:0.0,
+            front_spread:0.0,
+            back_spread:0.0,
+            top_roundness:0.0,
+            bottom_roundness:0.0,
+            height_scale:0.0,
+            height_offset:0.0
+        }
+    }
+}
+
+impl Default for Turret{
+    fn default() -> Turret{
+        Turret {
+            manual_control: true,
+            elevator: 0.0
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub enum Part {
+    Normal(BasePart),
+    AdjustableHull(BasePart,AdjustableHull),
+    Turret(BasePart,Turret),
+}
+
+pub trait HasBasePart{
+    fn base_part(&self) -> &BasePart;
+    fn base_part_mut(&mut self) -> &mut BasePart;
+}
+
+impl HasBasePart for Part{
+    fn base_part(&self) -> &BasePart {
+        match self {
+            Part::Normal(part) => part,
+            Part::AdjustableHull(part,_) => part,
+            Part::Turret(part,_) => part
+        }
+    }
+    fn base_part_mut(&mut self) -> &mut BasePart {
+        match self {
+            Part::Normal(part) => part,
+            Part::AdjustableHull(part,_) => part,
+            Part::Turret(part,_) => part
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ParseError {
+    desc: String
+}
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f,"{}",self.desc)
+    }
+}
+
+impl std::error::Error for ParseError {}
+
+fn get_attribute_string<'a>(e: &'a BytesStart<'a>, field_name: &str) -> Result<String, Box<dyn std::error::Error>> {
+    println!("checking the {:?} which was {:?}", field_name, str::from_utf8(e.try_get_attribute(field_name)?.unwrap().value.as_ref()));
+
+    return Ok(str::from_utf8(e.try_get_attribute(field_name)?
+        .ok_or(ParseError{desc: format!("field missing").to_string()})?
+        .value.as_ref())?.to_string());
+}
+
+pub fn load_save(file_path: &str) -> Result<Vec<Part>, Box<dyn Error>> {
+    let xml = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("thing is {xml}");
+
+    let mut reader = Reader::from_str(xml.as_str());
+    //reader.config_mut().trim_text(true);
+
+    let mut parts: Vec<Part> = Vec::new();
+
+    let mut current_part: Part = Part::Normal(BasePart::default());
+    let re = Regex::new(r"([^A-Fa-f0-9])").unwrap();
+
+    // The `Reader` does not implement `Iterator` because it outputs borrowed data (`Cow`s)
+    loop {
+        // NOTE: this is the generic case when we don't know about the input BufRead.
+        // when the input is a &str or a &[u8], we don't actually need to use another
+        // buffer, we could directly call `reader.read_event()`
+
+        match reader.read_event() {
+            Err(e) => panic!("Error at position {}: {:?}", reader.error_position(), e),
+            // exits the loop when reaching end of file
+            Ok(Event::Eof) => {
+                break;
+            },
+
+            Ok(Event::Start(e)) => {
+                match e.name().as_ref() {
+                    b"part" => {
+                        for attribute_result in e.attributes() {
+                            let attribute = attribute_result?;
+                            match attribute.key.local_name().as_ref() {
+                                b"id" => {
+                                    current_part.base_part_mut().id = str::from_utf8(attribute.value.as_ref())?
+                                        .parse::<i32>()?;
+                                }
+                                b"ignorePhysics" => {
+                                    current_part.base_part_mut().ignore_physics = str::from_utf8(attribute.value.as_ref())?.to_lowercase().parse::<bool>()?
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                    _ => {
+                    }
+                }
+            }
+            Ok(Event::Empty(e)) => {
+                match e.name().as_ref() {
+                    b"data" => {
+                        if let Part::Normal(base_part) = current_part {
+                            current_part = Part::AdjustableHull(base_part, AdjustableHull::default());
+                        }
+                        if let Part::AdjustableHull(_,adjustable_hull) = &mut current_part {
+                            adjustable_hull.length= get_attribute_string(&e, "length")?.parse::<f32>()?;
+                            adjustable_hull.height= get_attribute_string(&e, "height")?.parse::<f32>()?;
+                            adjustable_hull.front_width= get_attribute_string(&e, "frontWidth")?.parse::<f32>()?;
+                            adjustable_hull.back_width= get_attribute_string(&e, "backWidth")?.parse::<f32>()?;
+                            adjustable_hull.front_spread= get_attribute_string(&e, "frontSpread")?.parse::<f32>()?;
+                            adjustable_hull.back_spread= get_attribute_string(&e, "backSpread")?.parse::<f32>()?;
+                            adjustable_hull.top_roundness= get_attribute_string(&e, "upCurve")?.parse::<f32>()?;
+                            adjustable_hull.bottom_roundness= get_attribute_string(&e, "downCurve")?.parse::<f32>()?;
+                            adjustable_hull.height_scale= get_attribute_string(&e, "heightScale")?.parse::<f32>()?;
+                            adjustable_hull.height_offset= get_attribute_string(&e, "heightOffset")?.parse::<f32>()?;
+                        }
+                    }
+
+                    b"turret" => {
+                        if let Part::Normal(base_part) = current_part {
+                            current_part = Part::Turret(base_part, Turret::default());
+                        }
+                        if let Part::Turret(_,turret) = &mut current_part {
+                            turret.manual_control = get_attribute_string(&e, "manualControl")?.to_lowercase().parse::<bool>()?;
+                            turret.elevator = get_attribute_string(&e, "evevator")?.parse::<f32>()?;
+                        }
+                    }
+                    b"position" => {
+                        current_part.base_part_mut().position.x = get_attribute_string(&e, "x")?.parse::<f32>()?;
+                        current_part.base_part_mut().position.y = get_attribute_string(&e, "y")?.parse::<f32>()?;
+                        current_part.base_part_mut().position.z = get_attribute_string(&e, "z")?.parse::<f32>()?;
+                    }
+                    b"rotation" => {
+                        current_part.base_part_mut().rotation.x = get_attribute_string(&e, "x")?.parse::<f32>()?;
+                        current_part.base_part_mut().rotation.y = get_attribute_string(&e, "y")?.parse::<f32>()?;
+                        current_part.base_part_mut().rotation.z = get_attribute_string(&e, "z")?.parse::<f32>()?;
+                    }
+                    b"scale" => {
+                        current_part.base_part_mut().scale.x = get_attribute_string(&e, "x")?.parse::<f32>()?;
+                        current_part.base_part_mut().scale.y = get_attribute_string(&e, "y")?.parse::<f32>()?;
+                        current_part.base_part_mut().scale.z = get_attribute_string(&e, "z")?.parse::<f32>()?;
+                    }
+                    b"color" => {
+                        println!("color is {:?}",re.replace_all(&get_attribute_string(&e, "hex")?,""));
+                        let color: u32 = u32::from_str_radix(re.replace_all(&get_attribute_string(&e, "hex")?,"").as_ref(), 16)?;
+                        current_part.base_part_mut().color = Color::srgb_u8((color >> 16) as u8,(color >> 8) as u8,(color >> 0) as u8);
+                    }
+                    _ => {}
+                }
+
+                
+
+
+
+            }
+            Ok(Event::End(e)) => {
+                //println!("the end is {:?}",e.name().as_ref());
+                match e.name().as_ref() {
+                    b"part" => {
+                        parts.push(current_part);
+                        current_part = Part::Normal(BasePart::default());
+                    }
+                    _ => {}
+                }
+            }
+
+            // There are several other `Event`s we do not consider here
+            _ => (),
+        }
+        // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
+        //buf.clear();
+    }
+    println!("JKLASDJFAKSD {:?}",parts.len());
+
+    for part in &parts {
+        println!("aaahhkajdfhkal {:?}",part);
+    }
+
+    return Ok(parts);
+}
